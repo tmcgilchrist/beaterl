e6d6f596241277917d34be6905f821a02da1b743
Tim McGilchrist <timmcgil@gmail.com>
2013-02-14 21:55:34 +1100

    WIP for application.

--- priv/static/js/beaterl.js
+++ priv/static/js/beaterl.js
@@ -1,3 +1,36 @@
-window.App = Ember.Application.create({});
+window.App = Ember.Application.create({
+
+  ApplicationView: Ember.View.extend({
+    templateName: 'application',
+    classNames: ['application-view']
+  }),
+  ApplicationController: Ember.Controller.extend({
+  }),
+
+  LibraryView: Em.View.extend({
+    templateName: 'library'
+  }),
+  LibraryController: Em.ArrayController.extend(),
+
+  MainView: Em.View.extend({
+    templateName: 'main'
+  }),
+  MainController: Em.Controller.extend(),
+  Router: Ember.Router.extend({
+    enableLogging: true,
+    root: Ember.Route.extend({
+      index: Ember.Route.extend({
+        route: '/',
+        connectOutlets: function(router, context) {
+          router.get('applicationController').connectOutlet('main', 'main');
+          router.get('applicationController').connectOutlet('library', 'library');
+        }
+      })
+    })
+  })
+});
+
+
+App.Albums = Ember.Object.extend();
 
 App.initialize();
