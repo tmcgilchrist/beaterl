5ff2a06747214c4147f23951578cb0555322d47f
Tim McGilchrist <timmcgil@gmail.com>
2013-02-14 21:44:10 +1100

    Work around for ChicagoWeb wanting to run everything through ErlyDTL.
    Lets read the file and return the iolist.

--- src/controller/beaterl_home_controller.erl
+++ src/controller/beaterl_home_controller.erl
@@ -2,4 +2,8 @@
 -compile(export_all).
 
 index('GET', []) ->
-    {ok, [{greeting, "Hello, world!"}]}.
+    %% Read file in and return it
+    {ok, Contents} = file:read_file("./priv/static/index.html"),
+    %% {ok, file_io_list}.
+    {output, Contents}.
+    %% {ok, []}.
