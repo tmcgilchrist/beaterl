be349f5ff2c0ceb17e2c07983551a183b09d2671
Tim McGilchrist <timmcgil@gmail.com>
2013-02-15 18:45:16 +1100

    Simple listing of albums from the fixtures.

--- priv/static/js/beaterl.js
+++ priv/static/js/beaterl.js
@@ -1,36 +1,56 @@
-window.App = Ember.Application.create({
+window.App = Ember.Application.create();
 
-  ApplicationView: Ember.View.extend({
-    templateName: 'application',
-    classNames: ['application-view']
-  }),
-  ApplicationController: Ember.Controller.extend({
-  }),
-
-  LibraryView: Em.View.extend({
-    templateName: 'library'
-  }),
-  LibraryController: Em.ArrayController.extend(),
-
-  MainView: Em.View.extend({
-    templateName: 'main'
-  }),
-  MainController: Em.Controller.extend(),
-  Router: Ember.Router.extend({
-    enableLogging: true,
-    root: Ember.Route.extend({
-      index: Ember.Route.extend({
-        route: '/',
-        connectOutlets: function(router, context) {
-          router.get('applicationController').connectOutlet('main', 'main');
-          router.get('applicationController').connectOutlet('library', 'library');
-        }
-      })
-    })
-  })
+// Router
+App.Router.map(function() {
+  this.resource('albums'); // '/#/albums'
 });
 
+App.AlbumsRoute = Ember.Route.extend({
+    model: function() {
+        return App.Album.find();
+    }
+});
 
-App.Albums = Ember.Object.extend();
+// Controller
+App.AlbumsController = Ember.ArrayController.extend();
 
-App.initialize();
+// Models
+App.Store = DS.Store.extend({
+    revision: 11,
+    adapter: 'DS.FixtureAdapter'
+});
+
+App.Album = DS.Model.extend({
+    name: DS.attr('string'),
+    artist: DS.attr('string')
+});
+
+App.Album.FIXTURES = [{
+    id: 1,
+    name: 'Bleach',
+    artist: 'Nirvana'
+},{
+    id: 2,
+    name: 'Ten',
+    artist: 'Pearl Jam'
+},{
+    id: 3,
+    name: 'Siamese Dream',
+    artist: 'Smashing Pumpkins'
+},{
+    id: 4,
+    name: 'Sleeping with ghosts',
+    artist: 'Placebo'
+},{
+    id: 5,
+    name: 'Ok Computer',
+    artist: 'Radiohead'
+},{
+    id: 6,
+    name: 'Dirty',
+    artist: 'Sonic Youth'
+},{
+    id: 7,
+    name: 'Definitely Maybe',
+    artist: 'Oasis'
+}];
