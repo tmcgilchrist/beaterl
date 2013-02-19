672832dc459ae0bca247e335aded51f10400f4f0
Tim McGilchrist <timmcgil@gmail.com>
2013-02-19 21:15:22 +1100

    Display list of albums and their details.

--- priv/static/js/beaterl.js
+++ priv/static/js/beaterl.js
@@ -2,7 +2,15 @@ window.App = Ember.Application.create();
 
 // Router
 App.Router.map(function() {
-  this.resource('albums'); // '/#/albums'
+    this.resource('albums', function() {
+        this.resource('album', {path: '/:album_id'});
+    });
+});
+
+App.IndexRoute = Ember.Route.extend({
+    redirect: function() {
+        this.transitionTo('albums');
+    }
 });
 
 App.AlbumsRoute = Ember.Route.extend({
@@ -14,6 +22,8 @@ App.AlbumsRoute = Ember.Route.extend({
 // Controller
 App.AlbumsController = Ember.ArrayController.extend();
 
+App.AlbumController = Ember.ObjectController.extend();
+
 // Models
 App.Store = DS.Store.extend({
     revision: 11,
@@ -22,35 +32,43 @@ App.Store = DS.Store.extend({
 
 App.Album = DS.Model.extend({
     name: DS.attr('string'),
-    artist: DS.attr('string')
+    artist: DS.attr('string'),
+    coverImage: DS.attr('string')
 });
 
 App.Album.FIXTURES = [{
     id: 1,
     name: 'Bleach',
-    artist: 'Nirvana'
+    artist: 'Nirvana',
+    coverImage: 'static/img/nirvana-bleach.jpg'
 },{
     id: 2,
     name: 'Ten',
-    artist: 'Pearl Jam'
+    artist: 'Pearl Jam',
+    coverImage: 'static/img/pearl-jam-ten.jpg'
 },{
     id: 3,
     name: 'Siamese Dream',
-    artist: 'Smashing Pumpkins'
+    artist: 'Smashing Pumpkins',
+    coverImage: 'static/img/smashing-pumpkins-siamese-dream.jpg'
 },{
     id: 4,
     name: 'Sleeping with ghosts',
-    artist: 'Placebo'
+    artist: 'Placebo',
+    coverImage: 'static/img/placebo-sleeping-with-ghosts.jpg'
 },{
     id: 5,
     name: 'Ok Computer',
-    artist: 'Radiohead'
+    artist: 'Radiohead',
+    coverImage: 'static/img/radiohead-ok-computer.jpg'
 },{
     id: 6,
     name: 'Dirty',
-    artist: 'Sonic Youth'
+    artist: 'Sonic Youth',
+    coverImage: 'static/img/sonic-youth-dirty.jpg'
 },{
     id: 7,
     name: 'Definitely Maybe',
-    artist: 'Oasis'
+    artist: 'Oasis',
+    coverImage: 'static/img/oasis-definitely-maybe.jpg'
 }];
