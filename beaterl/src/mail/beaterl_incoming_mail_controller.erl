dec60af712dda31a9f739263c55ea078730aa20e
Tim McGilchrist <timmcgil@gmail.com>
2013-04-02 21:25:11 +1100

    More cleanup.

--- beaterl/src/mail/beaterl_incoming_mail_controller.erl
+++
@@ -1,8 +0,0 @@
--module(beaterl_incoming_mail_controller).
--compile(export_all).
-
-authorize_(User, DomainName, IPAddress) ->
-    true.
-
-% post(FromAddress, Message) ->
-%    ok.
--- beaterl/src/src/controller/beaterl_home_controller.erl
+++
@@ -1,9 +0,0 @@
--module(beaterl_home_controller, [Req]).
--compile(export_all).
-
-index('GET', []) ->
-    %% Read file in and return it
-    {ok, Contents} = file:read_file("./priv/static/index.html"),
-    %% {ok, file_io_list}.
-    {output, Contents}.
-    %% {ok, []}.
